e58567a8c63ad55fb7b8cf015d7041e5848a168e
Nico <mamani.nicolas.1799@gmail.com>
2023-10-08 14:52:54 -0400

    descripcion sin imagenes

--- frontend/src/components/CreateEvent.js
+++ frontend/src/components/CreateEvent.js
@@ -5,6 +5,11 @@ import Navbar from './Navbar';
 import './css/CrearEvento.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import derImage from './images/der.png';
+import izqImage from './images/izq.png';
+import cenImage from './images/cen.png';
+import jusImage from './images/jus.png';
+
 const inputStyle = {
   width: '170px',
   height: '30px',
@@ -48,123 +53,204 @@ const CreateEvento = () => {
     });
     navigate('/');
   };
+  // Estados para el tamaño de fuente y la alineación del texto
+  const [fontSize, setFontSize] = useState(16); // Tamaño de fuente inicial
+  const [textAlign, setTextAlign] = useState('left'); // Alineación inicial
 
+  const handleFontSizeChange = (size) => {
+    setFontSize(size);
+  };
+
+  const handleTextAlignChange = (alignment) => {
+    setTextAlign(alignment);
+  };
   return (
     <div>
       <Navbar />
       <div className="container mt-5">
         <div className="row">
           <div className="col-md-8 mx-auto">
-            <div className="card cardd">
+            <div className="card">
               <div className="card-body tarjeta">
                 <div className="row">
                   <div className="col-md-12">
                     <h2 className="card-title text-center">Crear Evento</h2>
                   </div>
                 </div>
-                <form onSubmit={store} className="text-left">
-                  <div className="mb-3">
-                    <label htmlFor="nombreEvento" className="form-label">Nombre del Evento</label>
-                    <input
-                      value={nombre_evento} 
-                      onChange={ (e)=> setNombreEvento(e.target.value)}
-                      type="text"
-                      className="form-control"
-                      id="nombreEvento"
-                      name="nombreEvento"
-                      style={inputStyle}
-                    />
-                  </div>
-                  <div className="mb-3">
-                    <label className="form-label">Tipo de Evento</label>
-                    <div>
-                      <label>
+                <div className="row">
+                  <div className="col-md-6">
+                    <form onSubmit={store} className="text-left">
+                      <div className="mb-3">
+                        <label htmlFor="nombreEvento" className="form-label">Nombre del Evento</label>
                         <input
-                          type="radio"
-                          value="Reclutamiento"
-                          onChange={handleTipoEventoChange}
-                          checked={tipo_evento === 'Reclutamiento'}
+                          value={nombre_evento}
+                          onChange={(e) => setNombreEvento(e.target.value)}
+                          type="text"
+                          className="form-control"
+                          id="nombreEvento"
+                          name="nombreEvento"
+                          style={inputStyle}
                         />
-                        Reclutamiento
-                      </label>
-                    </div>
-                    <div>
-                      <label>
+                      </div>
+                      <div className="mb-3">
+                        <label className="form-label">Tipo de Evento</label>
+                        <div>
+                          <label>
+                            <input
+                              type="radio"
+                              value="Reclutamiento"
+                              onChange={handleTipoEventoChange}
+                              checked={tipo_evento === 'Reclutamiento'}
+                            />
+                            Reclutamiento
+                          </label>
+                        </div>
+                        <div>
+                          <label>
+                            <input
+                              type="radio"
+                              value="Taller de reclutamiento"
+                              onChange={handleTipoEventoChange}
+                              checked={tipo_evento === 'Taller de reclutamiento'}
+                            />
+                            Taller de reclutamiento
+                          </label>
+                        </div>
+                        <div>
+                          <label>
+                            <input
+                              type="radio"
+                              value="Competencia de entrenamiento"
+                              onChange={handleTipoEventoChange}
+                              checked={tipo_evento === 'Competencia de entrenamiento'}
+                            />
+                            Competencia de entrenamiento
+                          </label>
+                        </div>
+                        <div>
+                          <label>
+                            <input
+                              type="radio"
+                              value="Competencia interna"
+                              onChange={handleTipoEventoChange}
+                              checked={tipo_evento === 'Competencia interna'}
+                            />
+                            Competencia interna
+                          </label>
+                        </div>
+                      </div>
+                      <div className="mb-3">
+                        <label htmlFor="fechaInicio" className="form-label">Fecha de Inicio</label>
                         <input
-                          type="radio"
-                          value="Taller de reclutamiento"
-                          onChange={handleTipoEventoChange}
-                          checked={tipo_evento === 'Taller de reclutamiento'}
+                          type="date"
+                          className="form-control"
+                          id="fechaInicio"
+                          name="fechaInicio"
+                          style={inputStyle}
+                          value={fecha_inicio}
+                          onChange={handleFechaInicioChange}
                         />
-                        Taller de reclutamiento
-                      </label>
-                    </div>
-                    <div>
-                      <label>
+                      </div>
+                      <div className="mb-3">
+                        <label htmlFor="fechaFin" className="form-label">Fecha de Fin</label>
                         <input
-                          type="radio"
-                          value="Competencia de entrenamiento"
-                          onChange={handleTipoEventoChange}
-                          checked={tipo_evento === 'Competencia de entrenamiento'}
+                          type="date"
+                          className="form-control"
+                          id="fechaFin"
+                          name="fechaFin"
+                          style={inputStyle}
+                          value={fecha_fin}
+                          onChange={handleFechaFinChange}
                         />
-                        Competencia de entrenamiento
-                      </label>
+                      </div>
+                      <div className="mb-3">
+                        <label htmlFor="horas" className="form-label">Horas</label>
+                        <select
+                          className="form-select"
+                          id="horas"
+                          name="horas"
+                          style={inputStyle}
+                          value={descripcion}
+                          onChange={handleHorasChange}
+                        >
+                          <option value="">Seleccionar horas</option>
+                          <option value="1">1 hora</option>
+                          <option value="2">2 horas</option>
+                          <option value="3">3 horas</option>
+                          <option value="4">4 horas</option>
+                        </select>
+                      </div>
+                      <button type="submit" className="btn btn-primary">Guardar</button>
+                    </form>
+                  </div>
+                  <div className="col-md-6 mx-auto">
+                    <div className="mb-3">
+                      <label htmlFor="descripcion" className="form-label">Descripción</label>
+                      <div className="mb-3">
+
+                        <div className="btn-group me-2">
+                          <button
+                            onClick={() => handleFontSizeChange(fontSize + 2)}
+                            className="btn btn-light btn-lg"
+                          >
+                            <strong>A</strong>
+                          </button>
+                          <button
+                            onClick={() => handleFontSizeChange(fontSize - 2)}
+                            className="btn btn-light btn-sm"
+                          >
+                            <small>A</small>
+                          </button>
+                        </div>
+                        <div className="btn-group">
+                          <button
+                            onClick={() => handleTextAlignChange('left')}
+                            className={`btn btn-light ${textAlign === 'left' ? 'active' : ''
+                              }`}
+                          >
+                            Izquierda
+                          </button>
+                          <button
+                            onClick={() => handleTextAlignChange('center')}
+                            className={`btn btn-light ${textAlign === 'center' ? 'active' : ''
+                              }`}
+                          >
+                            Centro
+                          </button>
+                          <button
+                            onClick={() => handleTextAlignChange('right')}
+                            className={`btn btn-light ${textAlign === 'right' ? 'active' : ''
+                              }`}
+                          >
+                            Derecha
+                          </button>
+                          <button
+                            onClick={() => handleTextAlignChange('justify')}
+                            className={`btn btn-light ${textAlign === 'justify' ? 'active' : ''
+                              }`}
+                          >
+                            Justificado
+                          </button>
+                        </div>
+                      </div>
+                      <textarea
+                        className="form-control"
+                        id="descripcion"
+                        name="descripcion"
+                        value={descripcion}
+                        onChange={handleHorasChange}
+                        rows="4"
+                        style={{
+                          fontSize: `${fontSize}px`,
+                          textAlign: textAlign,
+                          width: '100%',
+                          height: '200px',
+                          resize: 'none',
+                        }}
+                      ></textarea>
                     </div>
-                    <div>
-                      <label>
-                        <input
-                          type="radio"
-                          value="Competencia interna"
-                          onChange={handleTipoEventoChange}
-                          checked={tipo_evento === 'Competencia interna'}
-                        />
-                        Competencia interna
-                      </label>
-                    </div>
                   </div>
-                  <div className="mb-3">
-                    <label htmlFor="fechaInicio" className="form-label">Fecha de Inicio</label>
-                    <input
-                      type="date"
-                      className="form-control"
-                      id="fechaInicio"
-                      name="fechaInicio"
-                      style={inputStyle}
-                      value={fecha_inicio}
-                      onChange={handleFechaInicioChange}
-                    />
-                  </div>
-                  <div className="mb-3">
-                    <label htmlFor="fechaFin" className="form-label">Fecha de Fin</label>
-                    <input
-                      type="date"
-                      className="form-control"
-                      id="fechaFin"
-                      name="fechaFin"
-                      style={inputStyle}
-                      value={fecha_fin}
-                      onChange={handleFechaFinChange}
-                    />
-                  </div>
-                  <div className="mb-3">
-                    <label htmlFor="horas" className="form-label">Horas</label>
-                    <select
-                      className="form-select"
-                      id="horas"
-                      name="horas"
-                      style={inputStyle}
-                      value={descripcion}
-                      onChange={handleHorasChange}
-                    >
-                      <option value="">Seleccionar horas</option>
-                      <option value="1">1 hora</option>
-                      <option value="2">2 horas</option>
-                      <option value="3">3 horas</option>
-                      <option value="4">4 horas</option>
-                    </select>
-                  </div>
-                  <button type="submit" className="btn btn-primary">Guardar</button>
-                </form>
+                </div>
               </div>
             </div>
           </div>
