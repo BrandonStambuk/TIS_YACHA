c5da9004ac97f4e2a570813fdd932d1cf892e6be
Nico <mamani.nicolas.1799@gmail.com>
2023-10-19 10:20:57 -0400

    mensajes rojos en login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./css/Navbar.css";
 import "./css/fondo.css";
@@ -6,19 +6,39 @@ import Navbar from "./Navbar";
 import "./css/Login.css";
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!email.includes("@")) {
+      setMessage("Correo no válido");
+      return;
+    }
+    if (email === "admin@gmail.com" && password === "admin") {
+      setMessage("Inicio de sesión exitoso como administrador");
+      // Redirigir
+    } else {
+      setMessage("Los datos no pertenecen a ningún administrador");
+    }
+  };
   return (
     <div>
       <Navbar />
       <div className="container login">
         <div className="heading">Iniciar Sesion  ICPC UMSS</div>
-        <form action="" className="form">
+        <form onSubmit={handleSubmit} className="form">
           <input
             required=""
             className="input"
-            type="email"
             name="email"
             id="email"
             placeholder="Ingrese nombre de usuario"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             required=""
@@ -27,9 +47,11 @@ const Login = () => {
             name="password"
             id="password"
             placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <span className="forgot-password">
-            
+            {message && <span style={{ color: "red" }}>{message}</span>}
           </span>
           <input className="login-button" type="submit" value="Iniciar Sesion" />
           <label>Si olvido la contraseña contactese a la empresa</label>
