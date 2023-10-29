d0f8ed54aaa871f859e955df58eb16029966964b
Nico <mamani.nicolas.1799@gmail.com>
2023-10-29 17:30:51 -0400

    Home Page

--- frontend/src/components/HomePageUser.js
+++ frontend/src/components/HomePageUser.js
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import Navbar from './Navbar';
 import './css/Homepage.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import imagen1 from '../components/images/mi_afiche.png';
+import imagen1 from '../components/images/cabeza.jpg';
 import { Link } from 'react-router-dom';
 
 const endpoint = 'http://localhost:8000/api';
 
 const HomePage = () => {
+  const containerRef = useRef();
   const [eventos, setEventos] = useState([]);
 
   useEffect(() => {
@@ -19,37 +20,47 @@ const HomePage = () => {
     const response = await axios.get(`${endpoint}/mostrarPublico`);
     setEventos(response.data);
   };
+
+  const scrollContainer = (scrollAmount) => {
+    const container = containerRef.current;
+    container.scrollLeft += scrollAmount;
+  };
+
   return (
     <div>
       <Navbar />
       <div className="container mt-5">
         <div className="row">
-          <div className="col-md-8">
+          <div className="col-md-12">
             <div className="card card-translucent">
               <h3 className="card-header">Competencia Local UMSS</h3>
-              <div className="card-body">
+              <div ref={containerRef} className="card-body event-container">
                 {eventos.map((evento) => (
-                <div className="row mt-3">                  
-                  <div className="col-md-6">
-                    <img src={imagen1} alt="Afiche de Evento" />
+                  <div className="mt-3">
+                    <div className="image-container">
+                      <img src={imagen1} alt="Cabeza" />
+                      <p className="test">{evento.nombre_evento}</p>
+                    </div>
+                    <div className="card-footer bg-white shadow">
+                      <div className="event-info">
+                        <p className="event-info-text left"><strong>Tipo de evento: </strong>{evento.tipo_evento}</p>
+                        <div className="row">
+                          <p className="event-info-text left col-md-8"><strong>Inicio: </strong>{evento.fecha_inicio}</p>
+                          <p className="event-info-text left col-md-4"><strong></strong>{evento.hora} Horas</p>
+                        </div>
+                        <Link to={`/registroEvento/${evento.id}`} className="btn btn-info">inscripción</Link>
+                      </div>
+                    </div>
                   </div>
-                  <div className="col-md-6">                    
-                    <div className="event-info">
-                      <p className="event-info-text left"> <strong>Nombre Evento: </strong>{evento.nombre_evento}</p>
-                      <p className="event-info-text left"> <strong>Tipo de evento: </strong>{evento.tipo_evento}</p>
-                      <p className="event-info-text left"> <strong>Inicio del evento: </strong>{evento.fecha_inicio}</p>
-                      <p className="event-info-text left"><strong>Duracion: </strong>{evento.hora} Horas </p>  
-                      <p className="event-info-text left"> <strong>Descripción: </strong>{evento.descripcion}</p>
-                      <Link to={`/registroEvento/${evento.id}`} className='btn btn-info'>inscripción</Link>                   
-                    </div>                    
-                  </div>                  
-                </div>
                 ))}
               </div>
             </div>
           </div>
-          
         </div>
+      </div>
+      <div className="scroll-buttons">
+        <button onClick={() => scrollContainer(-100)}>&lt;</button>
+        <button onClick={() => scrollContainer(100)}>&gt;</button>
       </div>
     </div>
   );
