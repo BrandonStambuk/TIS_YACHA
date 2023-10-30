7b3fd4eb89cd5f8a5520eb4a166c35ea4ef91f25
JaruslavMark <87151030+Jarus3@users.noreply.github.com>
2023-10-30 19:13:09 -0400

    bugs mostrar evento creado
    Navbar

--- frontend/src/components/MostrarEventoUsuario.js
+++ frontend/src/components/MostrarEventoUsuario.js
@@ -73,10 +73,12 @@ const HomePage = () => {
                     </div>                    
                   </div>
           <div>
+          <p className="event-info-text left"> <strong>Descripción:</strong></p>
           <p className="event-info-text left"> {descripcion}</p>  
           </div>
           <div>
-          <Link to={`/registroEvento/${id}`} className='btn btn-info'>inscripción</Link> 
+          <Link to={`/home`} className='btn btn-danger'>Atras</Link> 
+          <Link to={`/registroEvento/${id}`} className='btn btn-info'>Ir a registro</Link> 
           </div>                  
         </div>
           
