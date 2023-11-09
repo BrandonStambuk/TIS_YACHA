859c72b9ba2f296c50bdbfcc70ca0f55d13b4308
BrandonStambuk <115127679+BrandonStambuk@users.noreply.github.com>
2023-11-09 16:37:26 -0400

    mejora del cerrar sesion

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   return (
     <div>
-      <Cabecera /> {/* Agrega el componente Cabecera al principio del Navbar */}
+      <div style={{ backgroundColor: 'RGB(242, 183, 5)', height: '15px' }}></div>
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
