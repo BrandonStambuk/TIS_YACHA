185eea8016d5af7d5c7269a3394fb3c3255fac07
Nico <mamani.nicolas.1799@gmail.com>
2023-11-18 20:38:48 -0400

    estilo basico en perfil

--- frontend/src/components/Perfil.js
+++ frontend/src/components/Perfil.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import editarIcon from './images/editar.png';
+import NavbarAdmin from './NavbarAdmin';
+import './css/Perfil.css'
 import { URL_API } from '../const';
 
 const Perfil = () => {
@@ -72,56 +74,71 @@ const Perfil = () => {
 
   return (
     <div>
-      <h2>Perfil de Usuario</h2>
-      <p>
-        <strong>Nombre:</strong> {userData.nombre}
-      </p>
-      <p>
-        <strong>Apellido:</strong> {userData.apellido}
-      </p>
-      <label>
-        <strong>Correo:</strong>{' '}
-        {editModeCorreo ? (
-          <span>
-            <input
-              type="email"
-              name="correo"
-              value={editedData.correo}
-              onChange={handleChange}
-            />
-            <button onClick={handleSaveClickCorreo}>Guardar</button>
-          </span>
-        ) : (
-          <span>
-            {userData.correo}
-            <button className="btn btn-transparent" onClick={handleEditClickCorreo}>
-              <img src={editarIcon} alt="Editar" style={editarIconStyle} />
-            </button>
-          </span>
-        )}
-      </label>
-      <br />
-      <label>
-        <strong>Contraseña:</strong>{' '}
-        {editModePassword ? (
-          <span>
-            <input
-              type="text"
-              name="password"
-              value={editedData.password}
-              onChange={handleChange}
-            />
-            <button onClick={handleSaveClickPassword}>Guardar</button>
-          </span>
-        ) : (
-          <span>
-            ********
-            <button className="btn btn-transparent" onClick={handleEditClickPassword}>
-              <img src={editarIcon} alt="Editar" style={editarIconStyle} />
-            </button>
-          </span>
-        )}
-      </label>
+      <NavbarAdmin />
+      <div className='row'>
+        <div className='container mt-5 container'>
+          <div className='card card-translucent col-md-6 mx-auto'>
+            <h2 className='card-header'>Perfil de Usuario</h2>
+            <div className='card-body'>
+              <div className='d-flex justify-content-center'>
+                <p className='border-right p-3'>
+                  <strong>Nombre:</strong> {userData.nombre}
+                </p>
+                <p className='border-left p-3'>
+                  <strong>Apellido:</strong> {userData.apellido}
+                </p>
+              </div>
+              <div>
+                <label>
+                  <strong>Correo:</strong>{' '}
+                  {editModeCorreo ? (
+                    <span>
+                      <input
+                        type="email"
+                        name="correo"
+                        value={editedData.correo}
+                        onChange={handleChange}
+                      />
+                      <button className='btn-save' onClick={handleSaveClickCorreo}>Guardar</button>
+                    </span>
+                  ) : (
+                    <span>
+                      {userData.correo}
+                      <button className="btn btn-transparent" onClick={handleEditClickCorreo}>
+                        <img src={editarIcon} alt="Editar" style={editarIconStyle} />
+                      </button>
+                    </span>
+                  )}
+                </label>
+              </div>
+              <div>
+                <label>
+                  <strong>Contraseña:</strong>{' '}
+                  {editModePassword ? (
+                    <span>
+                      <input
+                        type="text"
+                        name="password"
+                        value={editedData.password}
+                        onChange={handleChange}
+                      />
+                      <button className='btn-save' onClick={handleSaveClickPassword}>Guardar</button>
+                    </span>
+                  ) : (
+                    <span>
+                      ********
+                      <button className="btn btn-transparent" onClick={handleEditClickPassword}>
+                        <img src={editarIcon} alt="Editar" style={editarIconStyle} />
+                      </button>
+                    </span>
+                  )}
+                </label>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
