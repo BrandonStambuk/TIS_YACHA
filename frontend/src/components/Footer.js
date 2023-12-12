a9e063fd13bc3537a2242573a63c41d3a65610ab
Nico <mamani.nicolas.1799@gmail.com>
2023-12-11 20:36:46 -0400

    footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './css/Footer.css';
 const Footer = () => {
     return (
-        <footer className="bg-light bg-transparent">
+        <footer className="footer-user text-white ">
             <div class="container mt-4">
                 <div className='d-flex justify-content-between'>
                     <p>© {new Date().getFullYear()} YachayPachaSoft S.A.</p>
@@ -10,7 +10,7 @@ const Footer = () => {
                         <p>Contacto: +591 71234567</p>
                         <span className='mx-2'>|</span>
                         <a
-                            className="text-decoration-none text-dark"
+                            className="text-decoration-none text-white"
                             href='https://www.umss.edu.bo/'
                             target="_blank"
                             rel="noopener noreferrer">
