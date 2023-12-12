33fa5e7967ec12db7274822534421bf6162fbfc0
Nico <mamani.nicolas.1799@gmail.com>
2023-12-12 10:27:37 -0400

    notificacion de cambio

---
+++ BackendICPC/app/Notifications/ChangeNotification.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ChangeNotification extends Notification
+{
+    public $eventEditLink;
+
+    public function __construct($eventEditLink)
+    {
+        $this->eventEditLink = $eventEditLink;
+    }
+
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->greeting('Hola!')
+            ->subject('El evento al que te inscribiste ha cambiado')
+            ->line('Los organizaadores del evento han realizado cambios en el evento al que te inscribiste.')
+            ->action('Puedes ver los cambios aca', $this->eventEditLink)
+            ->line('Si no te inscribiste a ningun evento, puede ignorar este correo electrónico.')
+            ->line('¡Gracias por usar participar!');
+    }
+
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+}
