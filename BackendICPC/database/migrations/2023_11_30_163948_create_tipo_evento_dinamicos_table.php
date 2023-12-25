c5196e00063b30b0c75e3335098359582e75b260
JaruslavMark <87151030+Jarus3@users.noreply.github.com>
2023-12-25 12:47:01 -0400

    Update 2023_11_30_163948_create_tipo_evento_dinamicos_table.php

--- BackendICPC/database/migrations/2023_11_30_163948_create_tipo_evento_dinamicos_table.php
+++ BackendICPC/database/migrations/2023_11_30_163948_create_tipo_evento_dinamicos_table.php
@@ -16,6 +16,7 @@ class CreateTipoEventoDinamicosTable extends Migration
         Schema::create('tipo_evento_dinamicos', function (Blueprint $table) {
             $table->id();
             $table->string('nombre_tipo_evento_dinamico');
+            $table->boolean('tieneNota')->default(false);
             $table->timestamps();
         });
     }
